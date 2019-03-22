a2ac1b4e59ff8f6f790ce0138993fff2adc57686
Saikat Patra <Saikat.Patra@p10.com>
2019-03-22 17:21:03 +0530

    add one program

---
+++ multiple.java
@@ -0,0 +1,18 @@
+package frist;
+
+public class multiple {
+
+	public static void main(String[] args) {
+	
+			int res=mul(20,30);
+			System.out.println(res);
+			}
+		 public static int mul(int a, int b)
+			{
+				int c = a*b;
+				return c;
+			}
+			
+
+	}
+
